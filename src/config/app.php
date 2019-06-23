f5cdc47a7a5c821447a59aa4c74aa01e9a367f93
伊藤 大智 <daich125912@gmail.com>
2019-06-23 11:09:23 +0900

    [feat]scaffoldの設定

--- src/config/app.php
+++ src/config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Laralib\L5scaffold\GeneratorsServiceProvider::class ,
+
     ],
 
     /*
