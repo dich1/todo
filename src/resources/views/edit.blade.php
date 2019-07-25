c9117aed83c121785c29e4d5e1831b699cf2720e
伊藤 大智 <daich125912@gmail.com>
2019-07-25 17:10:58 +0900

    [fix]コミット編集画面の完了済内容の判定修正

--- src/resources/views/edit.blade.php
+++ src/resources/views/edit.blade.php
@@ -73,7 +73,7 @@
                   </div>
                 </div>
                 <div class="form-group @if($errors->has('content')) has-error @endif">
-                @if ($commit->status)
+                @if ($commitGroup->status)
                   <p id="content-field-{{ $commitGroup->id }}" class="completion-txt"><span>{{ is_null(old("content")) ? $commitGroup->content : old("content") }}</span></p>
                 @else
                   <input type="text" id="content-field-{{ $commitGroup->id }}" name="content[]" class="form-control" value="{{ is_null(old("content")) ? $commitGroup->content : old("content") }}"/>
