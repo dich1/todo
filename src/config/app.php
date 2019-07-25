9f26b47113909413e70b58b2488bfc3daf26cb14
伊藤 大智 <daich125912@gmail.com>
2019-07-25 18:19:40 +0900

    [fix]不要な指定のコメントアウト

--- src/config/app.php
+++ src/config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Laralib\L5scaffold\GeneratorsServiceProvider::class ,
+        // Laralib\L5scaffold\GeneratorsServiceProvider::class ,
 
     ],
 
