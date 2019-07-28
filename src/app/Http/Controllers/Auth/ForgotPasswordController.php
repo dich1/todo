1583335f29e5dd802e2059568ecc19a8107d9bb1
伊藤 大智 <daich125912@gmail.com>
2019-07-28 13:15:13 +0900

    [WIP][feat] メール送信処理のカスタマイズ

--- src/app/Http/Controllers/Auth/ForgotPasswordController.php
+++ src/app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use App\Mail\CustomEmail;
 
 class ForgotPasswordController extends Controller
 {
@@ -28,5 +29,12 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    public function sendResetLinkEmail(Request $request)
+    {
+
+        $data = ['message' => 'This is a test!'];
+        Mail::to($request->only('email'))->send(new CustomEmail($data));
     }
 }
