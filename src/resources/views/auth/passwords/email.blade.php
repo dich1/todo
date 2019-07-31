4dab58fda9a086d48033e83a5ecfd756a5af390c
伊藤 大智 <daich125912@gmail.com>
2019-08-01 08:17:53 +0900

    [feat] パスワード発行画面のデザイン当て

--- src/resources/views/auth/passwords/email.blade.php
+++ src/resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,43 @@
 @extends('layouts.app')
+@section('title')
+  <title>新しいパスワード発行 | Commit</title>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+  <main class="sec mainpage-form-main" id="password_reset">
+    <div class="wrap600">
+      <div class="mainpage-form form">
+        <h2>新しいパスワード発行</h2>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="form-wrap">
+          @if (session('status'))
+              <div class="alert alert-success" role="alert">
+                  {{ session('status') }}
+              </div>
+          @endif
+          
+          <p class="password_reset-txt">登録しているメールアドレスを入力してください。新しいパスワードを作成するためのリンクをメールでお送りします。</p>
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+          <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+            <div class="">
+              <div class="form-block">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+              </div>
             </div>
+
+            <div class="form-submit">
+              <input type="submit" name="" value="新しいパスワードを発行" class="link">
+            </div>
+          </form>
         </div>
+      </div>
     </div>
-</div>
+  </main>
 @endsection
