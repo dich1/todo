8cc2bade22e258d3f3a20c41c431adfce8bc3351
伊藤 大智 <daich125912@gmail.com>
2019-08-01 12:03:35 +0900

    [feat]トップ画面のデザイン当て

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -7,121 +7,25 @@
 @endsection
 
 @section('content')
-    @include('error')
-  <div id="single">
+  <div id="top">
     <div class="wrap600 ptb40-80">
-      <h1>2019.12.1までの20コミット</h1>
-
-      <div class="time-limit">
-        <div class="">
-          <span>残り</span><span>203</span><span>日で</span><span>20</span><span>個</span>
-        </div>
+      <div class="msg">
+        <p>Commitは目標、to do、やりたいことなどを<br class="pc">共有&管理できるサービスです。</p>
       </div>
 
-      <ol>
-        <li class="complete"><span class="txt">超有名な美容室で髪を切ってみる</span><span class="complete-txt"><span>完了</span></span></li>
-        <li><span>マッチングアプリで人と会ってみる</span></li>
-        <li class="complete"><span class="txt">超有名な美容室で髪を切ってみる</span><span class="complete-txt"><span>完了</span></span></li>
-        <li class="complete"><span class="txt">超有名な美容室で髪を切ってみる</span><span class="complete-txt"><span>完了</span></span></li>
-        <li><span>〜(国内)に行ってみる</span></li>
-        <li><span>1人旅してみる</span></li>
-        <li><span>体脂肪10%切ってみる</span></li>
-        <li><span>Y4GYMでパーソナル受けてみる</span></li>
-        <li><span>〜のライブに行ってみる</span></li>
-        <li><span>K1を観に行ってみる</span></li>
-        <li><span>両親に旅行をプレゼントしてみる</span></li>
-        <li><span>落語を見に行ってみる</span></li>
-        <li><span>吉本新喜劇を見に行ってみる</span></li>
-        <li><span>誰かにサプライズしてみる</span></li>
-        <li><span>一流の寿司屋に行ってみる</span></li>
-        <li><span>血液クレンジングやってみる</span></li>
-        <li><span>Twitterでフォロワーを1000人以上にしてみる</span></li>
-        <li><span>ホワイトニングしてみる</span></li>
-        <li><span>ギャンブルをやってみる</span></li>
-        <li><span>富士山に登ってみる</span></li>
-        <li><span>超有名な美容室で髪を切ってみる</span></li>
-        <li><span>マッチングアプリで人と会ってみる</span></li>
-        <li><span>スキューバダイビングしてみる</span></li>
-        <li><span>〜(海外)に行ってみる</span></li>
-        <li><span>〜(国内)に行ってみる</span></li>
-        <li><span>1人旅してみる</span></li>
-        <li><span>体脂肪10%切ってみる</span></li>
-        <li><span>Y4GYMでパーソナル受けてみる</span></li>
-        <li><span>〜のライブに行ってみる</span></li>
-        <li><span>K1を観に行ってみる</span></li>
-        <li><span>両親に旅行をプレゼントしてみる</span></li>
-        <li><span>落語を見に行ってみる</span></li>
-        <li><span>吉本新喜劇を見に行ってみる</span></li>
-        <li><span>誰かにサプライズしてみる</span></li>
-        <li><span>一流の寿司屋に行ってみる</span></li>
-        <li><span>血液クレンジングやってみる</span></li>
-        <li><span>Twitterでフォロワーを1000人以上にしてみる</span></li>
-        <li><span>ホワイトニングしてみる</span></li>
-        <li><span>ギャンブルをやってみる</span></li>
-        <li><span>富士山に登ってみる</span></li>
-        <li><span>超有名な美容室で髪を切ってみる</span></li>
-        <li><span>マッチングアプリで人と会ってみる</span></li>
-        <li><span>スキューバダイビングしてみる</span></li>
-        <li><span>〜(海外)に行ってみる</span></li>
-        <li><span>〜(国内)に行ってみる</span></li>
-        <li><span>1人旅してみる</span></li>
-        <li><span>体脂肪10%切ってみる</span></li>
-        <li><span>Y4GYMでパーソナル受けてみる</span></li>
-        <li><span>〜のライブに行ってみる</span></li>
-        <li><span>K1を観に行ってみる</span></li>
-        <li><span>両親に旅行をプレゼントしてみる</span></li>
-        <li><span>落語を見に行ってみる</span></li>
-        <li><span>吉本新喜劇を見に行ってみる</span></li>
-        <li><span>誰かにサプライズしてみる</span></li>
-        <li><span>一流の寿司屋に行ってみる</span></li>
-        <li><span>血液クレンジングやってみる</span></li>
-        <li><span>Twitterでフォロワーを1000人以上にしてみる</span></li>
-        <li><span>ホワイトニングしてみる</span></li>
-        <li><span>ギャンブルをやってみる</span></li>
-        <li><span>富士山に登ってみる</span></li>
-        <li><span>超有名な美容室で髪を切ってみる</span></li>
-        <li><span>マッチングアプリで人と会ってみる</span></li>
-        <li><span>スキューバダイビングしてみる</span></li>
-        <li><span>〜(海外)に行ってみる</span></li>
-        <li><span>〜(国内)に行ってみる</span></li>
-        <li><span>1人旅してみる</span></li>
-        <li><span>体脂肪10%切ってみる</span></li>
-        <li><span>Y4GYMでパーソナル受けてみる</span></li>
-        <li><span>〜のライブに行ってみる</span></li>
-        <li><span>K1を観に行ってみる</span></li>
-        <li><span>両親に旅行をプレゼントしてみる</span></li>
-        <li><span>落語を見に行ってみる</span></li>
-        <li><span>吉本新喜劇を見に行ってみる</span></li>
-        <li><span>誰かにサプライズしてみる</span></li>
-        <li><span>一流の寿司屋に行ってみる</span></li>
-        <li><span>血液クレンジングやってみる</span></li>
-        <li><span>Twitterでフォロワーを1000人以上にしてみる</span></li>
-        <li><span>ホワイトニングしてみる</span></li>
-        <li><span>ギャンブルをやってみる</span></li>
-        <li><span>富士山に登ってみる</span></li>
-        <li><span>超有名な美容室で髪を切ってみる</span></li>
-        <li><span>マッチングアプリで人と会ってみる</span></li>
-        <li><span>スキューバダイビングしてみる</span></li>
-        <li><span>〜(海外)に行ってみる</span></li>
-        <li><span>〜(国内)に行ってみる</span></li>
-        <li><span>1人旅してみる</span></li>
-        <li><span>体脂肪10%切ってみる</span></li>
-        <li><span>Y4GYMでパーソナル受けてみる</span></li>
-        <li><span>〜のライブに行ってみる</span></li>
-        <li><span>K1を観に行ってみる</span></li>
-        <li><span>両親に旅行をプレゼントしてみる</span></li>
-        <li><span>落語を見に行ってみる</span></li>
-        <li><span>吉本新喜劇を見に行ってみる</span></li>
-        <li><span>誰かにサプライズしてみる</span></li>
-        <li><span>一流の寿司屋に行ってみる</span></li>
-        <li><span>血液クレンジングやってみる</span></li>
-        <li><span>Twitterでフォロワーを1000人以上にしてみる</span></li>
-        <li><span>ホワイトニングしてみる</span></li>
-        <li><span>ギャンブルをやってみる</span></li>
-        <li><span>富士山に登ってみる</span></li>
-      </ol>
+      <div class="top-link">
+        <a href="{{ route('register') }}" class="registration">新規登録</a>
+        <a href="{{ route('login') }}" class="login">ログイン</a>
+      </div>
 
+      <div class="description">
+        <ul>
+          <li>目標決めてをSNSで宣言する。</li>
+          <li>チームでto doを共有する</li>
+          <li>自己満で使う</li>
+        </ul>
+        <p>などなど使い方は自由です。<br>簡単に管理画面でCommitの管理や共有が可能です。</p>
+      </div>
     </div>
   </div>
-
 @endsection
