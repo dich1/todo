bca1c295a22943f81b2fff097c5bae29b3815061
伊藤 大智 <daich125912@gmail.com>
2019-08-02 08:20:34 +0900

    [fix]ルートにメール認証の設定を追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('index');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::put('/home/{user_id}', 'HomeController@update')->name('home.update');
