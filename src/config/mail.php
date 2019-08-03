3fc6895fe77339580720cf7edba0deacf6970652
伊藤 大智 <daich125912@gmail.com>
2019-08-03 19:53:24 +0900

    [fix]メールの設定

--- src/config/mail.php
+++ src/config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'commit-20@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', '20.commit@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Commit'),
     ],
 
