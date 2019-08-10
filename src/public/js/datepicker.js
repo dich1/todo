03853ec4a3a4543d2aff653dba4689b9a3967e01
伊藤 大智 <daich125912@gmail.com>
2019-08-11 01:23:14 +0900

    [fix]datepickerの日本語化

--- src/public/js/datepicker.js
+++ src/public/js/datepicker.js
@@ -1,7 +1,10 @@
-$.getScript("https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/js/bootstrap-datepicker.min.js", function(){
-  $('.date-picker').datepicker({
-    format: 'yyyy-mm-dd',
-    autoclose: true,
-    startDate: '0'
-  });
+$.getScript("https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.1/js/bootstrap-datepicker.min.js", function(){
+    $.getScript("https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.1/locales/bootstrap-datepicker.ja.min.js", function(){
+        $('.date-picker').datepicker({
+            format: 'yyyy-mm-dd',
+            language: 'ja',
+            autoclose: true,
+            startDate: '0'
+        });
+    });
 });
