f27ecab7867ff5e802b70bd7b123780ffaea643b
伊藤 大智 <daich125912@gmail.com>
2019-08-10 22:47:48 +0900

    [fix]datepickerのフォーマット変更

--- src/public/js/datepicker.js
+++ src/public/js/datepicker.js
@@ -1,6 +1,6 @@
 $.getScript("https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/js/bootstrap-datepicker.min.js", function(){
   $('.date-picker').datepicker({
-    format: 'yyyy/mm/dd',
+    format: 'yyyy-mm-dd',
     autoclose: true,
     startDate: '0'
   });
