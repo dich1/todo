be1c2ba0ac16380f0c71ecffc3428d674775b10d
伊藤 大智 <daich125912@gmail.com>
2019-08-15 10:42:50 +0900

    [fix]カレンダーテーマの変更

--- src/public/js/datepicker.js
+++ src/public/js/datepicker.js
@@ -1,4 +1,4 @@
-$('head link:last').after('<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1/themes/pepper-grinder/jquery-ui.css">');
+$('head link:last').after('<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1/themes/blitzer/jquery-ui.css">');
 $.getScript("//ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js", function(){
     $.getScript("//ajax.googleapis.com/ajax/libs/jqueryui/1/i18n/jquery-ui-i18n.min.js", function(){
         $.datepicker.setDefaults($.datepicker.regional["ja"]);
