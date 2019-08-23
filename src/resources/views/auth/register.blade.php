8da56962e352f3cc7ba26741a79e3cabbbc7cd2f
伊藤 大智 <daich125912@gmail.com>
2019-08-23 18:04:01 +0900

    [fix]htmlバリデーションの追加

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -38,7 +38,7 @@
               </div>
               <div class="form-block">
                 <p class="form-bloc-item-name">メールアドレス<span class="required">*</span></p>
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="メールアドレスは、aaa@example.com のような形式で記入してください。" required autocomplete="email">
 
                 @error('email')
                     <span class="invalid-feedback" role="alert">
@@ -49,7 +49,7 @@
 
               <div class="form-block">
                 <p class="form-bloc-item-name">パスワード(8文字以上)<span class="required">*</span></p>
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required placeholder="例:8文字以上" autocomplete="new-password">
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" pattern="^(?=.*?[a-zA-Z])(?=.*?\d)(?=.*?[!-/:-@[-`{-~])[!-~]{8,100}$" title="パスワードは、半角英数字記号をそれぞれ1種類以上含む8文字以上16文字以下で記入してください。" required placeholder="例:8文字以上" autocomplete="new-password">
 
                 @error('password')
                     <span class="invalid-feedback" role="alert">
@@ -60,7 +60,7 @@
 
               <div class="form-block">
                 <p class="form-bloc-item-name">パスワード(確認用)<span class="required">*</span></p>
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" oninput="checkPassword(this)" required autocomplete="new-password">
               </div>
             </div>
 
@@ -80,4 +80,17 @@
       </div>
     </div>
   </main>
+@endsection
+@section('scripts')
+  <script>
+    function checkPassword(confirm){
+        var input1 = document.getElementById('password').value;
+        var input2 = confirm.value;
+        if(input1 != input2){
+            confirm.setCustomValidity("入力値が一致しません。");
+        }else{
+            confirm.setCustomValidity('');
+        }
+    }
+  </script>>
 @endsection
