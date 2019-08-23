d19c5d99688937327711142911cb3f0dfeeb80cf
伊藤 大智 <daich125912@gmail.com>
2019-08-23 17:24:01 +0900

    [fix]footerの灰色にするように修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -59,7 +59,7 @@
     </div>
     </header>
     @yield('content')
-    <footer id="footer" class="wrap">
+    <footer id="footer">
         <small>© Copyright 2019 All rights reserved.</small>
     </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
