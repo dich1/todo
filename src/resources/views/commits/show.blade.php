fb0c3f06802a898c7b53a6081155d0140bb20190
伊藤 大智 <daich125912@gmail.com>
2019-10-09 10:03:04 +0900

    [fix]みんなが見る画面のhtml変更

--- src/resources/views/commits/show.blade.php
+++ src/resources/views/commits/show.blade.php
@@ -2,20 +2,27 @@
 @section('title')
   <title>{{date('Y.m.d', strtotime($commit->limit))}}までの{{count($commit->commitGroups)}}コミット | Commit</title>
 @endsection
-@section('css')
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.css" rel="stylesheet">
-@endsection
 
 @section('content')
     @include('error')
   <div id="single">
     <div class="wrap600 ptb40-80">
-      <h1>{{date('Y.m.d', strtotime($commit->limit))}}までの{{count($commit->commitGroups)}}コミット</h1>
+      <h1><span>{{date('Y.m.d', strtotime($commit->limit))}}</span>までの<span>{{count($commit->commitGroups)}}</span>コミット</h1>
 
       <div class="time-limit">
         <div class="">
           <span>残り</span><span>{{ $remainingDays }}</span><span>日で</span><span>{{ $remainingCommits }}</span><span>個</span>
         </div>
+      </div>
+
+      <div class="shea">
+        <a id="twitter" class="shea-btn shea-btn-twitter">
+          <i class="fab fa-twitter"></i>
+        </a>
+
+        <a id="facebook" class="shea-btn shea-btn-facebook">
+          <i class="fab fa-facebook-f"></i>
+        </a>
       </div>
 
       <ol>
@@ -28,22 +35,21 @@
       @endforeach
       </ol>
 
+      <div class="btn-wrap">
+        @if (Auth::check() && Auth::id() === $commit->user_id && strtotime($commit->limit) >= strtotime(date("Y-m-d")))
+          <a href="{{ route('commits.edit', $commit->id) }}" class="edit-btn">編集する</a>
+        @endif
+        @if (Auth::check() && Auth::id() === $commit->user_id)
+          <a href="{{ route('commits.destroy', $commit->id) }}" class="delete-btn">削除する</a>
+        @endif
+      </div>
+
     </div>
   </div>
 
-  <div id="copy" data-clipboard-text="{{ url()->current() }}">コピーする</div>
-  <a id="twitter">twitter</a>
-  <a id="facebook">Facebook</a>
-  @if (Auth::check() && Auth::id() === $commit->user_id && strtotime($commit->limit) >= strtotime(date("Y-m-d")))
-  <a href="{{ route('commits.edit', $commit->id) }}" class="single-edit-btn">
-    <span>
-      <span>編集する</span>
-    </span>
-  </a>
-  @endif
-
 @endsection
 @section('scripts')
+  <script src="//kit.fontawesome.com/dbfcc583ce.js" crossorigin="anonymous"></script>
   @if(app('env') == 'local')
     <script src="{{ asset('js/share.js') }}" defer></script>
   @else
