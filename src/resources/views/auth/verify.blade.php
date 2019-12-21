c7fc86eb44c8e00d4b8e00003d832e37894d1833
伊藤 大智 <daich125912@gmail.com>
2019-12-22 04:58:23 +0900

    [fix] メール確認画面のデザイン当て

--- src/resources/views/auth/verify.blade.php
+++ src/resources/views/auth/verify.blade.php
@@ -1,24 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('message.verify.title') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('message.verify.new_url') }}
-                        </div>
-                    @endif
-
-                    {{ __('message.verify.send_action_url') }}
-                    @lang('message.verify.not_email', ['url' => route('verification.resend')])
-                </div>
+<main class="sec mainpage-form-main" id="signup_complete">
+  <div class="wrap600">
+    <div class="mainpage-form form">
+      <h2>{{ __('message.verify.title') }}</h2>
+        @if (session('resent'))
+            <div class="alert alert-success" role="alert">
+                {{ __('message.verify.new_url') }}
             </div>
-        </div>
+        @endif
+      <div class="form-wrap">
+        <form class="" action="index.html" method="post">
+          <p class="signup_complete-txt">{{ __('message.verify.send_action_url') }}
+                  @lang('message.verify.not_email', ['url' => route('verification.resend')])
+          </p>
+        </form>
+      </div>
     </div>
-</div>
+  </div>
+</main>
 @endsection
